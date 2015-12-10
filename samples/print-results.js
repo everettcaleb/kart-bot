de0ab0cb11870260d4341fc2867af7d36e626bbc
Caleb Everett <everettcaleb95@gmail.com>
2015-12-09 22:48:53 -0500

    More retrieval stuff, updated README, new data storage module

--- samples/print-results.js
+++ samples/print-results.js
@@ -1,5 +1,6 @@
 // Prints results of each kartlib call with test data
-var kartlib = require('../lib/kartlib.js');
+var kartlib = require('../lib/kartlib.js'),
+    kls = require('../lib/kartlib-storage.js');
 
 function f1() {
     console.log('kartlib.findRacerIdByName("F4$T3R_486")');
@@ -22,8 +23,31 @@ function f2() {
 function f3() {
     console.log('kartlib.getRacerHistoryById(2103578)');
     kartlib.getRacerHistoryById(2103578, function(data) {
-        console.log(data);
+        console.log(JSON.stringify(data));
+        console.log('');
+        f4();
     });
 }
 
-f1();
+function f4() {
+    console.log('kartlib.getHeatDetailsById(72964)');
+    kartlib.getHeatDetailsById(72964, function(data) {
+        console.log(JSON.stringify(data));
+        f5();
+    });
+}
+
+function f5() {
+    console.log('storage csv');
+    kartlib.getRacerHistoryById(2103578, function(data) {
+        kls.insertRacerHistory(data);
+        var csv = kls.getDataAsCsv();
+        console.log(csv.racers);
+        console.log('');
+        console.log(csv.heats);
+        console.log('');
+        console.log(csv.heatRacers);
+    });
+}
+
+f5();
